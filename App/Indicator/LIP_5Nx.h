bab21a6d239db98e286ff181aa2e750ff73d09f2
Loreleiyy <vadim-lorelei@yandex.ru>
2024-01-30 16:02:01 +0700

    добавлена таблица для перевода ascii в коды для индикатора

--- App/Indicator/LIP_5Nx.h
+++ App/Indicator/LIP_5Nx.h
@@ -7,7 +7,9 @@
 class LIP_5Nx : public Indicator
 {
 private:
-
+    bool typeKathode = false;
+    static const char ASCIITable[96];
+    uint8_t getChar(char symbol);
 public:
     LIP_5Nx();
     void setValue(uint8_t data[]);
