f255eec37056511ae14be317c034f997118ec266
Loreleiyy <vadim-lorelei@yandex.ru>
2024-03-29 13:55:14 +0700

    компилируемые классы IniParser и ISignal

---
+++ App/Parameters/signal.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+class TSlotHandlerArsg;
+
+class ISignal {
+public:
+struct PropsPointers{
+	char* dev;
+	char* pName;
+	char* pComment;
+	char* pType;
+	char* pOptional;
+};
+
+	ISignal(char* source, int scrLen);
+	ISignal(PropsPointers props);
+	virtual ~ISignal();
+	virtual std::string getName();
+	virtual std::string getValue(const TSlotHandlerArsg& args, const char* format);
+    virtual std::string getValue(){return "";};
+	virtual std::string getMSU();
+	virtual const std::string& getSignalType();
+	std::string getDefaultValue();
+protected:
+	char* Name;
+	char* optional;
+};
+
+//typedef ISignal* pSignal;
+
---
+++ App/ini/parser.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+enum class ParcerResult {
+	COMMENT = -1,
+	SECTION = -2,
+	END     = -3,
+	NOTKEYVALUE = -4
+};
+
+typedef struct {
+	char* tag;
+	int result;
+} TSectionReadResult;
+
+class IniParser  {
+public:
+	static IniParser& getInstance();
+	void init(void);
+	bool setSectionToRead(char* SectionName);
+	TSectionReadResult getNextTagString();
+	void setRoot(char* root, int size);
+	std::vector<std::string> getListOfDelimitedString(char delimiter, char* src, int size);
+	std::vector<std::string> getListOfDelimitedStrInclude(char delimiter, char* src, int size);
+	std::string getElement(char delimiter, char** ptr, int& size);
+	std::string getElement(char delimiter, char* ptr);
+	char* getElementPtrByNumber(int number, char delimiter, char* src);
+	int isDelimiterSizeLimited(char delimiter, char*& src, int& size);
+	int getStringLenght(char** ptr);
+private:
+	IniParser();
+	IniParser(const IniParser&) = delete;
+	IniParser& operator=(const IniParser&) = delete;
+	IniParser(const IniParser&&) = delete;
+	IniParser& operator=(const IniParser&&) = delete;
+
+	void resetFind(char* start);
+	char* getSectionEntryPoint(char* SectionName);
+	int getTagString(char** position);
+	int isDelimiter(char** ptr, char Delimiter);
+	int getSectionLinesCount(char* SectionName);
+	std::string getElementInclude(char delimiter, char** ptr, int& size);
+	char* Root;
+	char* SearchPointer;
+	int RootSize;
+};
+
