976bf900308d6d1c99726a9a5978c5cc19374f3c
Loreleiyy <vadim-lorelei@yandex.ru>
2024-04-02 11:28:35 +0700

    комипилируемый класс TU32BIT

---
+++ App/Parameters/TU32BIT.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "GeneralCaseSignal.h"
+
+class TU32BIT : public GeneralCaseSignal
+{
+private:
+	GeneralCaseSignal::RawReturn getRawValue(const TSlotHandlerArsg& args) override;
+	u32 string2raw(std::string& src);
+public:
+	TU32BIT(ISignal::PropsPointers props);
+	virtual const std::string getValueHex(std::string& src) override;
+	virtual InternalMemAddress getInternalMemAddr() override;
+};
+
--- App/main.cpp
+++ App/main.cpp
@@ -33,7 +33,7 @@
 #include <vector>
     
 
-#include "Parameters/TFloat.h"
+#include "Parameters/TU32BIT.h"
 /* defines ------------------------------------------------------------------*/
 //#define max_drebezg 0x0005//0x2000 // сколько раз нужно проверить нажатие кнопки для подавления дребезга контактов 
 /*---------------------------------------------------------------------------*/
@@ -85,7 +85,7 @@ int main(void)              //главная программа
   Page page;
 
   ISignal::PropsPointers props =  { nullptr, nullptr, nullptr, nullptr, nullptr };
-  TFloat fl(props);
+  TU32BIT u32tp(props);
 
 
   LED_RUN_ON;
