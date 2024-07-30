43ef0e60d32b8aa0f73aca09dc59a1a3279b17f3
Loreleiyy <vadim-lorelei@yandex.ru>
2024-07-30 16:47:45 +0700

    Чтение данных из устройства и обработка данных для индикатора SS8 в App

--- App/MCU/Indicator/LIP_SS8.h
+++ App/MCU/Indicator/LIP_SS8.h
@@ -2,12 +2,24 @@
 
 #include "Indicator.h"
 
+class Slot;
+
 class LIP_SS8 : public Indicator
 {
+private:
+    Slot* slot;
+    bool setIsignal();
+    void createReadCmd();
+    void clear();
+    std::string getValueStr();
+    Props parameter;
 public:
     LIP_SS8();
     ~LIP_SS8(){}
     std::vector<uint8_t> getValue() override;
+    void setParameter(std::string param) override;
     bool update() override;
+    void stopSlot() override;
+    void startSlot() override;
 };
 
