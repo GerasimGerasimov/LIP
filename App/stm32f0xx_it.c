0edafe73e04e4c7d4216e95d75f09a4a2d4a731d
Loreleiyy <vadim-lorelei@yandex.ru>
2024-08-02 09:31:14 +0700

    Перенос считывания DI кнопок в отдельный .cpp файл

--- App/stm32f0xx_it.c
+++ App/stm32f0xx_it.c
@@ -29,9 +29,6 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx_it.h"
-#include "DEFINES.h"
-#include "ramdata.h"
-#include "livecontrol.h"
 /** @addtogroup STM32F0xx_StdPeriph_Examples
   * @{
   */
@@ -40,26 +37,11 @@
   * @{
   */
 
-u16 SPI_DIO_Inputs;
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 
 /* Private macro -------------------------------------------------------------*/
-// CE - выбор сдвигового регистра
-// LOCK - защёлка
-
-// ST - состояние сигнала
-// UP - выставить сигнал в 1
-// DWN - выставить сигнал в 0  
-
-#define DI_CE_ST     (GPIOB->ODR & GPIO_Pin_15)
-#define DI_CE_UP     GPIO_SetBits(GPIOB, GPIO_Pin_15)
-#define DI_CE_DWN    GPIO_ResetBits(GPIOB, GPIO_Pin_15)
-
-#define DI_LOCK_ST   (GPIOB->ODR & GPIO_Pin_12)
-#define DI_LOCK_UP   GPIO_SetBits(GPIOB, GPIO_Pin_12)
-#define DI_LOCK_DWN  GPIO_ResetBits(GPIOB, GPIO_Pin_12)
 /* Private variables ---------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -71,60 +53,6 @@ u16 SPI_DIO_Inputs;
 void TIM1_CC_IRQHandler(void){
   if((TIM1->SR & TIM_FLAG_CC1) && (TIM1->DIER & TIM_IT_CC1)) TIM1_user_U1();
 
-}
-
-//считывание DI с кнопок
-void SPI_DIO_Processing(){
-  static bool isWaitReceive = false;
-  //если кристалл ещё не выбран CE в "1"
-  //сдвиговые регистры входов находятся в ресете
-  //инициализируем работу сдвиговых регистров
-  if(DI_CE_ST){
-      //если чип 74HC165 ещё не выбран, то сначала проверяю, в каком состоянии защёлка
-      //если защёлка не в нуле, то опускаю защёлку чтобы входы перешли в сдвиговый регистра
-    if(DI_LOCK_ST){//если защёлка в "1" 
-
-      DI_LOCK_DWN;//то ставлю в "0" на этом этапе денные из параллельного регистра переходят в последовательный
-    }
-    else{ //если защёлка в "0"
-
-      DI_LOCK_UP;//то ставлю её в "1" (т.е. возвращаю в исходное состояние)
-      DI_CE_DWN;//и выбираю 74HC165
-      isWaitReceive = false;
-    }
-  }
-  else{//чип 74HC165 уже выбран
-    if(!isWaitReceive){//если ещё не жду отправки (с параллельным приёмом!)
-
-      SPI2->DR = 0xFFFF;// //то оптправить по SPI единицы, чтобы в ответ получить состояние дискретных входов
-      isWaitReceive = true;
-    }
-    else{
-   //на всякий случай
-      while(SPI_I2S_GetFlagStatus(SPI2, SPI_I2S_FLAG_BSY) == SET){
-
-      };
-        //RAM_DATA.UoutAve++;
-        //прошло 0.001 сек, за это время данные должны быть получены
-      DI_CE_UP;//"освобождаю" кристалл
-      DI_LOCK_UP;
-      isWaitReceive = false;
-
-      SPI_DIO_Inputs = ~(SPI_I2S_ReceiveData16(SPI2));
-      /* (InputsPolarity == DIO_MODE_NORMAL)
-                          ? SPI_DIO->DR
-                          : ~(SPI_DIO->DR); */
-
-    }
-  }
-}
-
-void TIM2_IRQHandler(){
-  TIM2->SR = 0;
-
-  SPI_DIO_Processing();
-  RAM_DATA.DI = SPI_DIO_Inputs;
-  ctrlSysLive();
 }
 
 /**
