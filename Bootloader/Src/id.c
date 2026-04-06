7ef500dda9d427554c35b517d735351c4f795a2f
Loreleiyy <vadim-lorelei@yandex.ru>
2026-04-06 15:30:29 +0700

    Bootloader: Уменьшен размер Список секторов для прошивальщика генерируется на ходу, sprintf не используется

--- Bootloader/Src/id.c
+++ Bootloader/Src/id.c
@@ -1,4 +1,4 @@
 #include "id.h"
 
 /*__attribute__ ((section ("ID")))*/
-const char DeviceID[] = "00000002 Bootloader LIP v2.1.0 06.04.2026 www.intmash.ru";
+const char DeviceID[] = "00000002 Bootloader LIP v2.2.0 06.04.2026 www.intmash.ru";
